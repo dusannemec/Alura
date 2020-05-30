2f6120fba0711d000dd41c57bd555c2ae7536cc5
Rodrigo Lins <rodrigolins@protonmail.com>
2020-05-29 21:44:36 -0300

    Alocaçao dinamica de memoria

--- C: 3/fogefoge.c
+++ C: 3/fogefoge.c
@@ -1,15 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// matriz 5 x 10
+char** mapa;
+int linhas;
+int colunas;
+
 int main() {
-    // matriz 5 x 10
-    char mapa[5][10+1];
 
     FILE* f = fopen("mapa.txt", "r");
     if(f == 0) {
         printf("Erro na leitura do mapa\n");
         exit(1);
     }
+
+    fscanf(f, "%d %d", &linhas, &colunas);
+    printf("linhas %d colunas %d\n", linhas, colunas);
+
+    //////////////////
+    mapa = malloc(sizeof(char*) * linhas);
+    for (int i = 0; i < linhas; i++) {
+        mapa[i] = malloc(sizeof(char) * (colunas+1));
+    }
+
+    //////////////////
 
     for (int i = 0; i < 5; i++) {
         fscanf(f, "%s", mapa[i]);
@@ -19,4 +33,8 @@ int main() {
         printf("%s\n", mapa[i]);
     }
 
+    for (int i = 0; i < linhas; i++) {
+        free(mapa[i]);
+    }
+    free(mapa);
 }
