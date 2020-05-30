2e8c983f86c81fa7ab5bd7cabd3546082b799fc9
Rodrigo Lins <rodrigolins@protonmail.com>
2020-05-29 22:48:25 -0300

    Separando arquivos e utilizando ponteiros de structs

--- C: 3/fogefoge.h
+++ C: 3/fogefoge.h
@@ -1,16 +1,4 @@
-void liberamapa();
-void lemapa();
-void alocamapa();
-
 void ache (int* x, int* y);
 void move(char direcao);
 int acabou();
 void imprimemapa();
-
-struct mapa {
-    char** matriz;
-    int linhas;
-    int colunas;
-};
-
-typedef struct mapa MAPA;
