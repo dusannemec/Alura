f5d48876d73c93d5e73e23307cc7f78930c7bd15
Rodrigo Lins <rodrigolins@protonmail.com>
2020-06-02 10:28:59 -0300

    Finalizando curso 2 de C

---
+++ C-II/forca.h
@@ -0,0 +1,10 @@
+#define TAMANHO_PALAVRA 20
+
+void abertura();
+void chuta();
+int jachutou(char letra);
+void desenhaforca();
+int enforcou();
+int acertou();
+void adicionapalavra();
+int chuteserrados();
